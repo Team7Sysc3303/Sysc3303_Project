8ffb72c510a4c5337113ed751675390891d0e18a
huSin <hussainaljabri@cmail.carleton.ca>
2016-05-17 20:57:25 -0400

    Update ResponseHandler.java

--- iteration2/ResponseHandler.java
+++ iteration2/ResponseHandler.java
@@ -44,28 +44,34 @@ public class ResponseHandler implements Runnable {
 		
 	}//constructor
 	
+	   //////////////////////////////////////////////////////////////////////////////////////////////////////
+	   //////////////////////////////////////////////////////////////////////////////////////////////////////
+	   ////////////////////////////////////////////  Methods ////////////////////////////////////////////////
+	   //////////////////////////////////////////////////////////////////////////////////////////////////////
+	   //////////////////////////////////////////////////////////////////////////////////////////////////////
 	   /**
 	    * duplicate checks if the data/ack passed in parameter is 
 	    * duplicated or not
 	    * delayed or not
 	    * if both are not, then the received ack is bigger than what we expected.
 	    */
-	   public boolean duplicate(byte[] p, byte[] expected){
+	   public boolean verify(byte[] p, byte[] expected){
+	   if(p[0] == expected[0] && p[1] == expected[1]){
 		   if(p[2] == expected[2] && p[3] == expected[3]){
+			   
 			   // expected received, returning true.
-			   return true;
+			   return true; // same as expected.
+	
+		   }else if(p[2] < expected[2] ||( p[2] == expected[2] && p[3] < expected[3])){
 			   
-		   }else if(p[2] < expected[2] || p[3] < expected[3]){
-
-			   return false;
-			   
-		   }else{
-			   // the received ACK is bigger than the expected one.
-			   System.out.println("ACK received cannot be explained by delayed/duplicate errors.");
-			   System.out.println("Client: Shutting Down...");
-			   System.exit(1);
+			   return false; // duplicated.
+		   }
 		   }
 		   
+		   // the received ACK is bigger than the expected one.
+		   System.out.println("ACK received cannot be explained by delayed/duplicate errors.");
+		   System.out.println("Client: Shutting Down...");
+		   System.exit(1);
 		   return false;
 	   }
 	
@@ -98,6 +104,7 @@ public class ResponseHandler implements Runnable {
 	public boolean readRequestHandler() throws IOException{
 		byte[] data = new byte[516];
 		byte[] ack = new byte[4];
+		byte[] expACK = {0, 4, 0, 1};
 		data[0] = 0; data[1] = 3;
 		data[2] = 0; data[3] = 1;
 		boolean finished = false;
@@ -107,78 +114,85 @@ public class ResponseHandler implements Runnable {
 		// receivePacket going to act here only to receive "Ack's" therefore prepare it before loop.
 		receivePacket.setData(ack);
 		receivePacket.setLength(ack.length);
-		
+		boolean receivedIt;
 		while(!finished){
-			
+			receivedIt = false;
 			if((n=in.read(read)) != -1 ){
-				System.out.println("Value of n: " + n);
-
-				/**
-				 * SENDING & PRINTING DATA!
-				 */
-				try{
-					
-					System.arraycopy(read, 0, data, 4, read.length);
-					//--------printing what we got---------//
-					System.out.println("Server: Sending Data packet:");
-					System.out.println("To host: " + receivePacket.getAddress());
-					System.out.println("Destination host port: " + receivePacket.getPort());
-					int len = n+4;
-					System.out.println("Length: " + len);
-					System.out.println("Containing: ");
-					for (int j=0;j<len;j++) {
-						 System.out.print(data[j] + " ");
+				for(int i = 1; i<=5; i++){
+					/**
+					 * SENDING & PRINTING DATA!
+					 */
+					try{
+						System.arraycopy(read, 0, data, 4, read.length);
+						//--------printing what we got---------//
+						System.out.println("Server: Sending Data packet ["+ i +"]");
+						System.out.println("To host: " + receivePacket.getAddress());
+						System.out.println("Destination host port: " + receivePacket.getPort());
+						int len = n+4;
+						System.out.println("Length: " + len);
+						System.out.println("Containing: ");
+						for (int j=0;j<len;j++) {
+							 System.out.print(data[j] + " ");
+						}
+						//------------------------------------//
+						sendSocket.send(new DatagramPacket(data, n+4, receivePacket.getAddress(), receivePacket.getPort()));
+					}catch(IOException e){
+						e.printStackTrace();
+						System.exit(1);
 					}
-					//------------------------------------//
-					sendSocket.send(new DatagramPacket(data, n+4, receivePacket.getAddress(), receivePacket.getPort()));
-				}catch(IOException e){
-					e.printStackTrace();
-					System.exit(1);
+
+					/**
+					 * Wait & Print ACK's 
+					 */
+					try{
+						sendSocket.setSoTimeout(1000);
+						System.out.println("Server: Waiting ACK Packet[" + i +"]");
+						sendSocket.receive(receivePacket);
+						// verify if we received the expected block number or not.
+						if(verify(receivePacket.getData(), expACK)){
+							receivedIt = true;
+							expACK = receivePacket.getData();
+							updateBlockNum(data);
+							// getting the expected block number to wait for the new ACK to be compared to.
+							updateBlockNum(expACK);
+							break;
+						}
+					}catch(SocketTimeoutException e){
+						System.out.println("Waiting for ACK" + i + "  Timedout. ( " + (5-i) +" ) tries left");
+					}
+					//--------------------------//
 				}
+				// check if we break out the loop and received the intended packet or not.
+				// if we dont receive any packet and our tries expired. Server will giveup and quit.
+					if(!receivedIt){
+						System.out.println("Server: Did not receive Ack Packet.. Server Shutting Down..");
+						System.exit(1);
+					}
 				
-				/**
-				 * Wait & Print ACK's 
-				 */
-				try{
-					
-					System.out.println("Server: Waiting ACK Packet..");
-					 sendSocket.receive(receivePacket);
-				}catch(IOException e){
-					e.printStackTrace();
-					System.exit(1);
-				}
-				//--------------------------//
-				
-				/**
-				 * Checking and Printing Ack's
-				 */
-				// checking if its not Ack.
-				if(ack[1] != 4 && ack[0] != 0){
-					System.out.println("We received invalid ACK packet (OP-error!).");
-					System.exit(1);
+					/**
+					 * Checking and Printing Ack's
+					 */
+					// checking if its not Ack.
+					if(ack[1] != 4 && ack[0] != 0){
+						System.out.println("We received invalid ACK packet (OP-error!).");
+						System.exit(1);
+					}else{
+						/* prints the ack received */
+						System.out.println("Server: Acknowledge packet received.");
+						System.out.println("To host: " + receivePacket.getAddress());
+						System.out.println("Destination host port: " + receivePacket.getPort());
+						int len = receivePacket.getLength();
+						System.out.println("Length: " + len);
+						System.out.println("Containing: ");
+						for (int j=0;j<receivePacket.getLength();j++) {
+							 System.out.print(receivePacket.getData()[j] + " ");
+						}
+					}
+					System.out.println();
+					//------------------------------//
 				}else{
-					/* prints the ack received */
-					System.out.println("Server: Acknowledge packet received.");
-					System.out.println("To host: " + receivePacket.getAddress());
-					System.out.println("Destination host port: " + receivePacket.getPort());
-					int len = receivePacket.getLength();
-					System.out.println("Length: " + len);
-					System.out.println("Containing: ");
-					for (int j=0;j<receivePacket.getLength();j++) {
-						 System.out.print(receivePacket.getData()[j] + " ");
-					}
+					return true;
 				}
-				System.out.println();
-				//------------------------------//
-
-				// we need to update the data block #
-				updateBlockNum(data);
-
-				
-			}else{
-				
-				return true; // we finished.
-			}
 			
 		}// end while
 		
@@ -198,8 +212,8 @@ public class ResponseHandler implements Runnable {
 	public boolean writeRequestHandler(){
 		byte[] ack = {0,4,0,0};
 		byte[] data = new byte[516];
-		// since we're expecting datablock 01 for first data transfer, we might need to initialize oldData for first time
-		byte[] oldData = {0,3,0,1}; // it'll carry the block num of old data.
+		// since we're expecting data block 01 for first data transfer, we might need to initialize oldData for first time
+		byte[] expData = {0,3,0,1}; // it'll carry the block num of old data.
 		
 		
 		boolean finished = false;
@@ -259,7 +273,7 @@ public class ResponseHandler implements Runnable {
 			// get the data block number and transfer it to ack
 			System.arraycopy(receivePacket.getData(), 2, ack, 2, ack.length);
 
-			if(!duplicate(receivePacket.getData(), oldData)){
+			if(verify(receivePacket.getData(), expData)){
 				try {
 					out.write(receivePacket.getData(), 4, receivePacket.getLength()-4);
 				} catch (IOException e1) {
@@ -267,7 +281,7 @@ public class ResponseHandler implements Runnable {
 					e1.printStackTrace();
 				}
 				// get the block number.
-				System.arraycopy(receivePacket.getData(), 0, oldData, 0, oldData.length);
+				System.arraycopy(receivePacket.getData(), 0, expData, 0, expData.length);
 				/**
 				 * Check if we need to end this transfer connection
 				 */
@@ -340,11 +354,6 @@ public class ResponseHandler implements Runnable {
 			}
 		}
 			
-			
-			
-		
-		
-
 		// We're finished with this socket, so close it.
 		sendSocket.close();		
 	}//end of run
